47d5367317570dd20a8559fd888d38d37c424255
Erick Yudha <erickyudha18@gmail.com>
2022-09-30 16:08:26 +0700

    edit page done + bug fixing

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,30 @@
+const carSizeInput = new DropdownInput(
+    "Small",
+    "car-size-input",
+    "img/icons/input_dropdown.svg",
+    "carSize",
+    {
+        "Small": "small",
+        "Medium": "medium",
+        "Large": "large"
+    }
+)
+
+carSizeInput.render();
+carSizeInput.init();
+
+const carPicInput = new FileInput(
+    "pic-input",
+    "Click to select file...",
+    "img/icons/upload.svg",
+    "carImg",
+    "pic-input-validation"
+)
+carPicInput.render();
+carPicInput.init();
+
+
+
 // FORM VALIDATION
 const carNameInput = document.querySelector("#carName")
 const rentPerDayInput = document.querySelector("#rentPerDay")
@@ -25,6 +52,9 @@ const formValidation = () => {
     if (carNameInput.value === "") isValid = false;
     if (rentPerDayInput.value === "") isValid = false;
     if (picInput.value === "") isValid = false;
+
+    const sizeInputVal = document.querySelector(sizeInputSelectedSelector)
+    if (sizeInputVal === null) sizeInput[0].checked = true;
 
     return isValid;
 }
