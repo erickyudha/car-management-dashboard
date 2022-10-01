f31103158423f779b6044c67014779feb0005208
Erick Yudha <erickyudha18@gmail.com>
2022-10-01 16:45:01 +0700

    setup backend api, and router skeleton

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const host = "localhost"
-const port = 3000;
+const port = 4000;
 
 const fs = require('fs')
 const path = require("path")
 const morgan = require('morgan')
+
+const router = require("./src/routes");
+const handler = require("./src/handler");
+
 const express = require("express"),
     app = express();
 
@@ -12,28 +16,13 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'),
 
 // setup the logger
 app.use(morgan('combined', { stream: accessLogStream }))
-app.use(morgan('dev'));
-
-// static file serving
-app.use(express.static(path.join(__dirname, 'public')));
-//setting view engine to ejs
-app.set("view engine", "ejs");
+app.use(morgan("dev"))
+app.use(express.json());
 
 // ROUTERS
-app.get("/", function (req, res) {
-    res.render("index");
-});
-app.get("/add", function (req, res) {
-    res.render("add");
-});
-app.get("/edit", function (req, res) {
-    res.render("edit");
-});
-app.use('/edit', (req, res) => {
-    res.status(404);
-    res.send('<h1>404 NOT FOUND</h1>');
-});
+app.use("/", router);
 
+// START
 app.listen(port, function () {
     console.log(`Server is running on http://${host}:${port}`);
 });
---
+++ backend/src/routes.js
@@ -0,0 +1,65 @@
+const express = require('express');
+
+const carData = require("../test_data.json");
+
+const router = express.Router();
+
+// TODO: ADD ERROR HANDLING
+
+router.get('/', (req, res) => {
+    res.send({ message: 'Hello world' });
+});
+
+router.get('/cars', (req, res) => {
+    const resultData = [];
+    carData.forEach(car => {
+        const appendData = {
+            id: car.id,
+            name: `${car.manufacture} - ${car.model}`,
+            size: "small",
+            rentPerDay: car.rentPerDay,
+            image: car.image
+        }
+        resultData.push(appendData)
+    })
+
+    res.send({
+        data: resultData
+    })
+})
+
+router.get('/cars/:carId', (req, res) => {
+    const carId = req.params.carId;
+    const car = carData.find(car => car.id === carId);
+
+    res.send({
+        data: {
+            id: car.id,
+            name: `${car.manufacture} - ${car.model}`,
+            size: "small",
+            rentPerDay: car.rentPerDay,
+            image: car.image
+        }
+    })
+})
+
+router.post('/cars', (req, res) => {
+    // ADD NEW CAR TO DB
+    // make new car id
+    // add data to db
+    res.send("heyaa");
+})
+
+router.put('/cars/:carId', (req, res) => {
+    const carId = req.params.carId;
+    // edit car based on id in db
+    res.send("hyeooo")
+})
+
+router.delete('/cars/:carId', (req, res) => {
+    const carId = req.params.carId;
+    // delete car based on id in db
+    res.send("hyedwaadooo")
+})
+
+module.exports = router;
