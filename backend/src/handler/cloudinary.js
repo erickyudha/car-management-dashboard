a4cad2e91138d29f09b4bf3653e65ffdcf75a4c5
Erick Yudha <erickyudha18@gmail.com>
2022-10-07 13:26:10 +0700

    remove unused dependencies

--- backend/src/handler/cloudinary.js
+++ backend/src/handler/cloudinary.js
@@ -2,9 +2,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: "dgjwtquka",
-    api_key: "851982188236256",
-    api_secret: "DhkUsZjzVEC-ARzZgXYM9UMSNvA",
+    cloud_name: "CHANGE THIS",
+    api_key: "CHANGE THIS",
+    api_secret: "CHANGE THIS",
     secure: true,
 });
 
